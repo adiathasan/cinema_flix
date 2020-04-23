f6c846506400ebe7f20afbd8f86de8cae0175455
adiathasan <adiathasan.me@gmail.com>
2020-04-23 09:09:09 -0700

    changed time settings.

--- cp_django_movie_app/settings.py
+++ cp_django_movie_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'a_sgw7^4&vhc0)guovb_$ndjim7rfmj&1vf9xf1kb%)#@_&$(='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Dhaka'
 
 USE_I18N = True
 
